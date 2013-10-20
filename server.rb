19c4bafedca76807a5b00bd6073e5376ca5b1bce
Sebastian Kippe <sebastian@kip.pe>
2013-10-20 15:24:52 +0300

    Add CORS middleware, define endpoints

--- server.rb
+++ server.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
+require 'rack/cors'
 
-get '/' do
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', :headers => :any, :methods => [:get, :post]
+  end
+end
+
+get '/download' do
+  #TODO Deliver test file
   "Hello World!"
+end
+
+post '/upload' do
+  "Too fast, man"
 end
